427c12088d952900aa9ace0468f715a217e3c3de
Jimmy Eisenhauer <james.eisenhauer@gmail.com>
2022-10-03 22:21:12 -0700

    Initial Commit

---
+++ xcodeChild/xcodeChild.h
@@ -0,0 +1,18 @@
+//
+//  xcodeChild.h
+//  xcodeChild
+//
+//  Created by Jimmy Eisenhauer on 10/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for xcodeChild.
+FOUNDATION_EXPORT double xcodeChildVersionNumber;
+
+//! Project version string for xcodeChild.
+FOUNDATION_EXPORT const unsigned char xcodeChildVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xcodeChild/PublicHeader.h>
+
+
